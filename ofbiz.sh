25245b90da02b0df9e8d4ed69ceefc0099515575
Jerram Watters <jerramw@gmail.com>
2018-04-05 06:06:01 +1000

    Update standalone ofbiz install script with apt upgrade

--- ofbiz.sh
+++ ofbiz.sh
@@ -4,13 +4,12 @@ sudo debconf-set-selections <<< "debconf shared/accepted-oracle-license-v1-1 see
 export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt update -y
-# sudo apt upgrade -y
+sudo apt upgrade -y
 sudo apt -qq install -y \
   python-software-properties \
   oracle-java8-installer \
   wget \
   unzip
-# apt-get install -y --no-install-recommends default-jdk
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> ~/.bashrc
 source ~/.bashrc
 # Start Ofbiz
